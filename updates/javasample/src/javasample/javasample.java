e6008052c543a9d901b3e1f5a269fabdef9ef967
karthik <mailmadu@gmail.com>
2017-07-15 13:16:59 +0000

    initial commit

---
+++ updates/javasample/src/javasample/javasample.java
@@ -0,0 +1,10 @@
+package javasample;
+
+public class javasample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("Program has run successfully");
+	}
+
+}
