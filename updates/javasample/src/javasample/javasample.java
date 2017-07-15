158c1615eb28b0336d1fbaed4e43e353c4f3c187
karthik <mailmadu@gmail.com>
2017-07-15 13:25:25 +0000

    updated comments

--- updates/javasample/src/javasample/javasample.java
+++ updates/javasample/src/javasample/javasample.java
@@ -4,7 +4,7 @@ public class javasample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  System.out.println("Program has run successfully");
+		  System.out.println("Program has run successfully!!!");
 	}
 
 }
